6e861a807427331bc04e1acd58d1b95e4c79f744
Ha_Velop <62905985+khabib33@users.noreply.github.com>
2021-09-23 15:16:15 +0900

    Create variable.py

---
+++ variable.py
@@ -0,0 +1,17 @@
+x = 5 
+print(x)
+x = "Hello!"
+print(x)
+x = 7.9
+type_of_variable = type(x)
+print(type_of_variable)
+
+
+#Calculating of credit final sum
+
+credit = 1000
+credit_rate = 10
+number_of_years = 5
+final_sum = credit + credit / 100 * credit_rate * number_of_years
+
+print(final_sum)
